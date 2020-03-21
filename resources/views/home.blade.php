32b7b1c4ee1bcd9ff025df6f21e9682e67ecc278
nico <nicolanilomartino@yahoo.com>
2020-03-21 15:03:18 +0800

    installed passport

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,9 +9,9 @@
 
                 <div class="card-body">
                     @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
+                    </div>
                     @endif
 
                     You are logged in!
@@ -19,5 +19,8 @@
             </div>
         </div>
     </div>
+    <passport-clients></passport-clients>
+    <passport-authorized-clients></passport-authorized-clients>
+    <passport-personal-access-tokens></passport-personal-access-tokens>
 </div>
 @endsection
